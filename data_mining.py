adf2fad37949672901b8dc19d481a98fde2a4589
Arslan <arsymaster@gmail.com>
2022-08-27 13:56:33 +0300

    Add new function get_df_columns

---
+++ data_mining.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def get_df_shape(file_path):
+    df = pd.read_csv(file_path)
+    return df.shape
+
+def get_df_dtypes(file_path):
+    df = pd.read_csv(file_path)
+    return df.dtypes
+
+def get_df_columns(file_path):
+    df = pd.read_csv(file_path)
+    return df.columns
